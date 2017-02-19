8aa1540f333d7ba8d33aab7573e0a5f0213cef57
Dita Dewindita <dita.dewindita@gmail.com>
2017-02-18 23:57:34 -0500

    Login added, configured style

--- login/forms.py
+++ login/forms.py
@@ -15,7 +15,7 @@ class RegistrationForm(forms.Form):
         widget=forms.PasswordInput(attrs=dict(required=True, max_length=30, render_value=False)), label=_("Password"))
     password2 = forms.CharField(
         widget=forms.PasswordInput(attrs=dict(required=True, max_length=30, render_value=False)),
-        label=_("Password (again)"))
+        label=_("Re-enter password"))
 
 
     def clean_username(self):
@@ -23,11 +23,11 @@ class RegistrationForm(forms.Form):
             user = User.objects.get(username__iexact=self.cleaned_data['username'])
         except User.DoesNotExist:
             return self.cleaned_data['username']
-        raise forms.ValidationError(_("The username already exists. Please try another one."))
+        raise forms.ValidationError(_("This username already exists. Please try another one."))
 
     def clean(self):
         if 'password1' in self.cleaned_data and 'password2' in self.cleaned_data:
             if self.cleaned_data['password1'] != self.cleaned_data['password2']:
-                raise forms.ValidationError(_("The two password fields did not match."))
+                raise forms.ValidationError(_("Your passwords do not match."))
         return self.cleaned_data
 
